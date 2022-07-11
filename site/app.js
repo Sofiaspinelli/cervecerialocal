65dd00156a58f0d0c7f1c5c9320b00a3df961302
German704 <germaninsaurralde704@gmail.com>
2022-07-11 13:05:14 -0300

    modifico y arreglo algunos errores

--- site/app.js
+++ site/app.js
@@ -1,39 +1,37 @@
-/* Livereload */
+/* livereload */
 const livereload = require('livereload');
 const liveReloadServer = livereload.createServer();
 
 /* Entry point */
-const express = require('express')
-const connectLivereload = require('connect-livereload')
-const path = require('path')
+const express = require('express');
+const connectLivereload = require('connect-livereload');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3006;
 
 /* Archivos estaticos */
-app.use(express.static(path.resolve(__dirname,'public')))
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 /* Archivos estaticos monitoreados */
 liveReloadServer.watch(path.join(__dirname, 'public'));
 app.use(connectLivereload());
 
 /* Rutas */
-app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','index.html')))
-app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
-app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')))
-app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','detalle.html')))
-app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')))
-app.get('*',(req,res) => res.sendFile(path.resolve(__dirname,'views','404.html')))
-
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')));
+app.get('/detalle', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'detalle.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')));
+app.get('/carrito', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'carrito.html')));
+app.get('/productos', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'productos.html')))
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'views', '404.html')));
 
 /* Funcion de actualizacion del servidor */
 liveReloadServer.server.once("connection", () => {
-    setTimeout(() => {
-      liveReloadServer.refresh("/");
-    }, 100);
-  });
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
 
-/* Levantamos el servidor con app listen */
-app.listen(port,function(){
-    return console.log(`Se levanta el servidor en http://localhost:${port}`)
-})
+/* levantamos servidor */
+app.listen(port, () => console.log(`servidor levantado en el puerto ${port}`));
